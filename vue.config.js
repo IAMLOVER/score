276b6ce1160a09782c1c3d223b028f237848e466
zhuguo <757276716@qq.com>
2019-09-17 14:25:41 +0800

    创建新项目

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+function resolve(dir) {
+  console.log(path.join(__dirname, dir))
+  return path.join(__dirname, dir)
+};
+module.exports = {
+  lintOnSave: false, //关闭整个页面的eslint检查
+  productionSourceMap: false,
+  chainWebpack(config) {
+    config.resolve.alias
+      .set('components', resolve('src/components'))
+      .set('common', resolve('src/common'))
+      .set('api', resolve('src/api'))
+  }
+};
